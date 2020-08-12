48b076adccb858521fa68cee3e2b59ac4b597ae8
Kanak Agrawal <kanakagrawal600@gmail.com>
2020-08-12 19:51:24 +0530

    Navbar background on scroll
    Change background of nav bar to green on  scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,16 @@
         $('#preloader').delay(400).fadeOut('slow');
         $('body').delay(400).css({'overflow':'visible'});
     })
+
+
+
+/*----------nav bar----------*/
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20) {
+    document.getElementById("navbarNavAltMarkup").style.background = "green";
+  } else {
+    document.getElementById("navbarNavAltMarkup").style.background = "transparent";
+  }
+}
